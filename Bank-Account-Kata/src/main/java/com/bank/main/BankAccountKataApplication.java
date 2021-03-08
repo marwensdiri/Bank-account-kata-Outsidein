d0cff5fb98dd9cc7e025e32066468b56dfba672b
Marwen <sdiri_marwen@live.fr>
2021-03-08 14:20:01 +0100

    create main application

--- Bank-Account-Kata/src/main/java/com/bank/main/BankAccountKataApplication.java
+++ Bank-Account-Kata/src/main/java/com/bank/main/BankAccountKataApplication.java
@@ -1,13 +1,27 @@
 package com.bank.main;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.bank.domain.Account;
+import com.bank.repositories.TransactionRepository;
+import com.bank.service.Clock;
+import com.bank.service.Console;
+import com.bank.service.StatementPrinter;
 
 @SpringBootApplication
 public class BankAccountKataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankAccountKataApplication.class, args);
+		Clock clock = new Clock();
+		TransactionRepository transactionRepository = new TransactionRepository(clock );
+		Console console = new Console();
+		StatementPrinter statementPrinter = new StatementPrinter(console );
+		Account account = new Account(transactionRepository , statementPrinter ); 
+
+		account.deposit(1000);
+		account.withdraw(400);
+		account.withdraw(1200);
+		account.printStatement();
 	}
 
 }
